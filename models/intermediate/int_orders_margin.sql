0fcbca729522135ece594460258e52746d570658
Elise Gonthier <arkeoptys@gmail.com>
2025-10-21 14:21:30 +0000

    commit 7

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,10 +1,10 @@
 SELECT 
 DISTINCT (orders_id) AS orders_id
 , date_date
-, SUM (revenue) AS revenue
+, ROUND(SUM (revenue), 2) AS revenue
 , SUM (quantity) AS quantity
-, SUM (purchase_price) AS purchase_price
-, SUM(margin) AS margin 
+, ROUND(SUM (purchase_price), 2) AS purchase_price
+, ROUND(SUM(margin), 2) AS margin 
 FROM {{ ref('int_sales_margin') }}
 GROUP BY 
 date_date
